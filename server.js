b5acff615f1c98d4127dc89b0264b4cdc6c5b2c8
skinsler <simonkinsler@gmail.com>
2019-06-18 22:19:56 -0500

    updated model names and made fields unique

--- server.js
+++ server.js
@@ -37,21 +37,20 @@ mongoose.connect(MONGODB_URI);
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("http://www.twincities.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
+    console.log($);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $(".article-title").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("a")
-        .text();
+        .attr("title");
       result.link = $(this)
-        .children("a")
         .attr("href");
 
       // Create a new Article using the `result` object built from scraping
